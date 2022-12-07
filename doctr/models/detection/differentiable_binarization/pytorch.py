5405a893ed3f570ddf522e291377b32f22e612f0
Aashish Kumar <aashish.kumar1@byjus.com>
2022-12-07 17:15:49 +0530

    changes

--- doctr/models/detection/differentiable_binarization/pytorch.py
+++ doctr/models/detection/differentiable_binarization/pytorch.py
@@ -45,6 +45,11 @@ default_cfgs: Dict[str, Dict[str, Any]] = {
         "std": (0.264, 0.2749, 0.287),
         "url": "https://doctr-static.mindee.com/models?id=v0.4.1/db_resnet50-1138863a.pt&src=0",
     },
+    "db_resnet152": {
+        "input_shape": (3, 1024, 1024),
+        "mean": (0.798, 0.785, 0.772),
+        "std": (0.264, 0.2749, 0.287),
+    },
 }
 
 
